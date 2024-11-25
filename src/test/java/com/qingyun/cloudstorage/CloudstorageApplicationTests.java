d471eeafc7dbe73cce680c50b4dc8a947bb08690
Hades <zstbmw@163.com>
2024-11-25 22:51:03 +0800

    Spring Boot Base File

---
+++ src/test/java/com/qingyun/cloudstorage/CloudstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingyun.cloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
